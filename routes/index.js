4d7f5ea7ec14d382f77213ad40f6d7783465ff85
tomaspeld <tomaspeld@gmail.com>
2019-12-25 13:21:31 -0300

    cambios-htcc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET add product page. */
+router.get('/addProduct', function(req, res, next) {
+  res.render('addProduct', { title: 'product' });
+});
+
+/* GET add car page. */
+router.get('/carProduct', function(req, res, next) {
+  res.render('carProduct', { title: 'litle car' });
+});
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'registro' });
+});
+
+/* GET productDtaile page. */
+router.get('/ProductDetail', function(req, res, next) {
+  res.render("productdetail", { title: 'menu' });
 });
 
 module.exports = router;
