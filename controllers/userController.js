08af8a4e08e03a6f29c603e448deaafb280329d6
miguel andres <miguel_3715@hotmail.com>
2020-01-21 00:50:29 -0300

    ya el usuario se registra y guarda en un json

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,17 @@
 const fs = require("fs") ;
 const path = require ("path")
 const usuariosDatabase = path.join(__dirname , '../data/usuarios.json')
+const {check ,validationResult, body}= require("express-validator")
+const bcrypt = require("bcrypt")
 
 
-const userController ={
+
+
+
+
+
+
+const userController = {
 
     registro :function(req, res, next) {
         res.render('register', { title: 'registro' })
@@ -14,22 +22,75 @@ const userController ={
      },
 
     ingresoDatos: function(req,res,next){
-            console.log(req.body)
-            let usuario = req.body
+           console.log(validationResult(req))
+
+           let errors = validationResult(req)
+
+
+            if (errors.isEmpty()){
+                
+              
+           
+            let usuario ={
+                name : req.body.name,
+                lastName :req.body.lastName,
+                email : req.body.email,
+                password : bcrypt.hashSync(req.body.password,2) ,
+                confirmpassword : req.body.confirmpassword,
+            }
             let dataUsuarios = fs.readFileSync(usuariosDatabase,{encoding:"utf-8"})
             
             let usuarios = JSON.parse(dataUsuarios)
-            usuarios.push(usuario)
 
-            fs.writeFileSync(usuariosDatabase, JSON.stringify(usuarios))
+            for ( let i = 0 ; i < usuarios.length ;i++ ){
+                if (req.body.email == usuarios[i].email){
+                    res.render("register")
+                    return ;
+                }
+            }
+                    usuarios.push(usuario)
 
-            res.send("hola te has registrado") 
+                    fs.writeFileSync(usuariosDatabase, JSON.stringify(usuarios))
+        
+                    res.render("register" ,{ Bienvenido : req.body.name})
+                    
+         }/*else del if error*/ else{
+                return res.render("register", {errors : errors.errors})
+            }
+                   
+             
         } ,
 
     login: function(req,res,next){
+       
+        
 
-        console.log(req.body)
-        res.send("HOLA has ingresado a tu usuario ")},
+        let dataUsuarios = fs.readFileSync(usuariosDatabase,{encoding:"utf-8"})
+
+        let usuarios = JSON.parse(dataUsuarios)
+        
+
+        for (var i = 0 ; i < usuarios.length ;i++){
+
+            if (req.body.email == usuarios[i].email &&  req.body.password == usuarios[i].confirmpassword){ 
+            
+                res.render("userProfile")
+                return;
+            
+            } else {
+                res.send("el usuario no existe")
+                return ;
+            }
+        }
+
+        
+
+
+        
+
+        
+
+       },
 
 
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var productController = require("../controllers/productController");
 var userController = require("../controllers/userController.js")
+var {check,validationResult,body} = require("express-validator")
+var fs = require("fs")
+var path =require ("path")
+
+const usuariosDatabase = path.join(__dirname , '../data/usuarios.json')
+
+
+
 
 
 /* GET home page. */
@@ -40,7 +48,36 @@ router.get('/register', userController.registro);
 
 /*POST register page*/
 
-router.post("/register", userController.ingresoDatos)
+router.post("/register", [
+  check("email", "ingrese un email valido" ).isEmail(),
+  body("email").custom( value =>{ 
+    let dataUsuarios = fs.readFileSync(usuariosDatabase,{encoding:"utf-8"})
+    let usuarios = JSON.parse(dataUsuarios)
+    for (let i= 0; i<usuarios.length ; i++){
+      if(usuarios[i].email == value){
+        return false ;
+      }
+    }
+    return true ;
+
+  }).withMessage("usuario ya existente"),
+  check("name", "nombre invalido").isLength({min:1}),
+  check("lastName" ,"Apellido invalido").isLength({min:1}),
+  check("password","ingrese clave valida" ).isLength({min:3}).custom((value,{req, loc, path}) => {
+    if (value !== req.body.confirmpassword) {
+        // trow error if passwords do not match
+        throw new Error("Passwords don't match");
+    } else {
+        return value;
+    }
+}),
+  
+
+  
+  
+
+ 
+], userController.ingresoDatos)
 
 
 
