b9a4eed2b647a94f5b1ddfb3ab099764b2c51707
tomaspeld <tomaspeld@gmail.com>
2020-04-01 00:31:28 -0300

    validacion en products terminada

--- public/js/productsJS/add.js
+++ public/js/productsJS/add.js
@@ -1,123 +1,178 @@
-/*
 window.addEventListener("load", function(){
+    let form = document.querySelector("#form");
+    let formElements = Array.from(form.elements);
+    formElements.pop();
+    let inputErrors = {};
 
-  let form = document.querySelector("form.formulario");
+    for (let oneInput of formElements) {
+        oneInput.addEventListener("blur", function() {
+          let inputValue = this.value;
+          if (validator.isEmpty(inputValue)) {
 
-  form.addEventListener("submit", function(e) {
-      
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = `El campo <b>${this.name}</b> es obligatorio`
 
-      let errores =  [];
-      let campoCategoria = document.querySelector("input.categoria");
-     
-      if (campoCategoria.value == "") {
-          errores.push("el campo esta vacio");
-      }else if ((campoCategoria.value != "") && (campoCategoria.value.length < 3 )) {
-          errores.push("El campo debe tener mas de 3 caracteres")
-      }
-      
-      /*if (campoCategoria != String) {
-        errores.push("Campo de texto")
-      }*/
+              inputErrors[this.name] = true
 
-      /*
+          } else {
+           
+            this.classList.remove("is-invalid");
+            this.classList.add("is-valid");
 
-      let campoNombre = document.querySelector("input.nombre");
+            delete inputErrors[this.name];
+           
 
-      if (campoNombre.value == "") {
-          errores.push("el campo esta vacio");
-      } if ((campoNombre.value != "") && (campoNombre.value.length < 3 )) {
-          errores.push("El campo debe tener mas de 3 caracteres")
-      }
-      
-      /*if (campoNombre != String) {
-        errores.push("Campo de texto")
-      }*/
-
-      /*
-
-      let campoPrecio_individual = document.querySelector("input.precio_individual")
-
-      if(campoPrecio_individual.value == "" ) {
-        errores.push("el campo esta vacio")
-      } if ((campoPrecio_individual.value != "") && (campoPrecio_individual.value.length < 2 )) {
-        errores.push("El campo debe tener mas de 2 caracteres")
-      }
-      
-      /*if (campoPrecio_individual != Number) {
-        errores.push("Campo numérico")
-      }*/
-
-      /*
-
-      let campoPrecio_mediana = document.querySelector("input.precio_mediana")
-
-      if(campoPrecio_mediana.value == "" ) {
-        errores.push("el campo esta vacio")
-      } if ((campoPrecio_mediana.value != "") && (campoPrecio_mediana.value.length < 2 )) {
-        errores.push("El campo debe tener mas de 2 caracteres")
-      }
-      
-      /*if (campoPrecio_mediana != Number) {
-        errores.push("Campo numérico")
-      }*/
-
-      /*
-
-      let campoPrecio_grande = document.querySelector("input.precio_grande")
-
-      if(campoPrecio_grande.value == "" ) {
-        errores.push("el campo esta vacio")
-      } if ((campoPrecio_grande.value != "") && (campoPrecio_grande.value.length < 2 )) {
-        errores.push("El campo debe tener mas de 2 caracteres")
-      } 
-      
-      /*if (campoPrecio_grande != Number) {
-        errores.push("Campo numérico")
-      }*/
-
-      /*
-       
-      let campoDetalle = document.querySelector("input.detalle");
-
-      if (campoDetalle.value == "") {
-        errores.push("el campo esta vacio");
-    } if ((campoDetalle.value != "") && (campoDetalle.value.length < 3 )) {
-        errores.push("El campo debe tener mas de 3 caracteres")
-    } 
-    
-    /*if (campoDetalle != String) {
-        errores.push("Campo de texto")
-      }*/
-
-      /*
-
-      let campoProducto = document.querySelector("input.producto");
-
-      
-      if (campoProducto.value == "") {
-        errores.push("el campo esta vacio");
-    } if ((campoProducto.value != "") && (campoProducto.value.length < 3 )) {
-        errores.push("El campo debe tener mas de 3 caracteres")
-    }
-    
-    /*if (campoProducto != String) {
-        errores.push("Campo de texto")
-      }*/
-
-      /*
-      
-      if(errores.length > 0) {
-        e.preventDefault();
-
-        let  ulErrores = document.querySelector("div.errores ul")
-        for (let i = 0; i < errores.length; i++) {
+          }
+        });
           
-            ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
-        }
+
+          if(oneInput.name === "categoria"){
+            oneInput.addEventListener("blur",function(){
+             let inputCategoria = this.value;
+             if (!validator.isEmpty(inputCategoria) && !validator.isAlpha(inputCategoria)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo de texto"
+              inputErrors[this.name] = true
+
+          } 
+
+            })
+
+          }
+          
+          if(oneInput.name === "nombre"){
+            oneInput.addEventListener("blur",function(){
+             let inputNombre = this.value;
+             if (!validator.isEmpty(inputNombre) && !validator.isAlpha(inputNombre)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo de texto"
+              inputErrors[this.name] = true
+
+          } 
+
+            })
+
+          }
+          if(oneInput.name === "precio_individual"){
+            oneInput.addEventListener("blur",function(){
+             let inputPrecioInd = this.value;
+             if (!validator.isEmpty(inputPrecioInd) && !validator.isNumeric(inputPrecioInd)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo numérico"
+              inputErrors[this.name] = true
+
+           } 
+
+            })
+
+          }
+          if(oneInput.name === "precio_mediana"){
+            oneInput.addEventListener("blur",function(){
+             let inputPrecioMed = this.value;
+             if (!validator.isEmpty(inputPrecioMed) && !validator.isNumeric(inputPrecioMed)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo numérico"
+              inputErrors[this.name] = true
+
+           } 
+
+            })
+
+          }
+
+          if(oneInput.name === "precio_grande"){
+            oneInput.addEventListener("blur",function(){
+             let inputPrecioGran = this.value;
+             if (!validator.isEmpty(inputPrecioGran) && !validator.isNumeric(inputPrecioGran)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo numérico"
+              inputErrors[this.name] = true
+
+           } 
+
+            })
+
+          }
+
+          if(oneInput.name === "detalle"){
+            oneInput.addEventListener("blur",function(){
+             let inputDetalle = this.value;
+             if (!validator.isEmpty(inputDetalle) && validator.isNumeric(inputDetalle)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo de texto"
+              inputErrors[this.name] = true
+               } 
+
+            })
+
+          }
+
+          if(oneInput.name === "producto"){
+            oneInput.addEventListener("blur",function(){
+             let inputProducto = this.value;
+             if (!validator.isEmpty(inputProducto) && !validator.isAlpha(inputProducto)) {
+
+              this.classList.add("is-invalid");
+              this.classList.remove("is-valid");
+              this.nextElementSibling.innerHTML = "Campo de texto"
+              inputErrors[this.name] = true
+               } 
+
+            })
+
+          }
+
+           if( oneInput.name == "avatar"){
+               oneInput.addEventListener("change",function(){
+                let fileExtension = this.value.split(".").pop();
+                let accepteExtensions = ["jpg", "jpeg", "png", "webm", "svg"];
+                 if (!accepteExtensions.includes(fileExtension)) {
+ 
+                 this.classList.add("is-invalid");
+                 this.classList.remove("is-valid");
+
+                this.nextElementSibling.innerHTML = `Formato invalido, los formatos pueden ser: ${accepteExtensions}`;
+                inputErrors[this.name] = true
+
+               } else {
+                  this.classList.remove("is-invalid");
+                  this.classList.add("is-valid");
+                  this.nextElementSibling.innerHTML ="";
+                  delete inputErrors[this.name];
+
+                }
+
+              });
+            }
+      
     }
 
-       
-   });
-  })
+    form.addEventListener("submit",function(e){  
+      formElements.forEach(function(oneInput){
+        if(validator.isEmpty(oneInput.value)){
+          inputErrors[oneInput.name] = true;
+          oneInput.classList.add("is-invalid");
+          oneInput.nextElementSibling.innerHTML = "Campo obligatorio"
+        }
+      })
+      if(Object.keys(inputErrors).length > 0 ) {
+        e.preventDefault();
+    }
+    })
+ 
 
-*/
+  })
+  
